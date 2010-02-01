930ec1942aafb88621e0a86cb3213507b2ff839e
Reiner Pittinger <reiner.pittinger@neofonie.de>
2010-02-01 14:59:14 +0100

    Removed promo-subline to text posted via Facebook

--- weshare-mobile-iphone/src/Plugins/Facebook/WSFacebookPlugin.m
+++ weshare-mobile-iphone/src/Plugins/Facebook/WSFacebookPlugin.m
@@ -122,8 +122,6 @@
 	
 	NSMutableArray* actualParameters = [[NSMutableArray alloc] init];
 	
-	BOOL descriptionSet = NO;
-	
 	for (NSString* key in [streamParamsDict allKeys]) {
 		NSString* value = [self.dataDict objectForKey: key];
 		
@@ -135,20 +133,10 @@
 				value = [NSString stringWithFormat: @"[{\"type\":\"image\",\"src\":\"%@\",\"href\":\"%@\"}]", value, href];
 				
 				[actualParameters addObject: [NSString stringWithFormat: @"\"%@\":%@", streamKey, value]];
-			} else {
-				// WeShare self promotion
-				if ([streamKey isEqualToString: @"description"]) {
-					value = [NSString stringWithFormat: @"%@ (%@)", value, WSLocalizedString(@"Shared through WeShare", nil)];
-					descriptionSet = YES;
-				}
-				
+			} else {				
 				[actualParameters addObject: [NSString stringWithFormat: @"\"%@\":\"%@\"", streamKey, value]];
 			}
 		}
-	}
-	
-	if (!descriptionSet) {
-		[actualParameters addObject: [NSString stringWithFormat: @"\"%@\":\"%@\"", @"description", WSLocalizedString(@"Shared through WeShare", nil)]];
 	}
 	
 	NSString* parameterString = [actualParameters componentsJoinedByString: @","];
